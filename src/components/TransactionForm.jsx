00675d0e323f7d24fec5db1b1b0d983b6a36e254
Alfonso Bermudez <alfonso.bermudez@motorpor.pt>
2026-05-22 10:38:53 +0100

    fix: remove unused useEffect import from TransactionForm

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useCategories } from '../hooks/useCategories'
 import { useInstruments } from '../hooks/useInstruments'
 import { DEFAULT_PLATFORM, PLATFORMS } from '../lib/constants'
